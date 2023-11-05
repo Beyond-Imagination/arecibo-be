c296f34fc98235eba98a7dead870a65063260501
laggu91 <laggu91@gmail.com>
2023-11-05 10:01:28 +0000

    chore: initialize express
    Merge-request: ARECIBO-MR-2
    Merged-by: 구원 나 <laggu91@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+/* eslint-env node */
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+        'prettier',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended'
+    ],
+    "rules": {
+        "prettier/prettier": "error",
+        "no-unused-vars": ["error", {"argsIgnorePattern": "req|res|next|reject"}]
+    }
+}
