c015f90525ff4056103fbc6c2d82af98a825df87
재현 윤 <yuchem2@gmail.com>
2023-11-19 12:31:04 +0000

    feat: alien model
    Merge-request: ARECIBO-MR-10
    Merged-by: 재현 윤 <yuchem2@gmail.com>

---
+++ src/models/alien.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+import { getModelForClass, prop } from '@typegoose/typegoose'
+
+export class Alien {
+    public _id: mongoose.Types.ObjectId
+
+    @prop({ required: true, unique: true })
+    public nickname: string
+
+    @prop()
+    public oauthProvider: string
+
+    @prop()
+    public oauthId: string
+
+    @prop()
+    public subscribe: [string, ]
+
+    @prop()
+    public createdAt: Date
+
+    @prop()
+    public updatedAt: Date
+
+    @prop()
+    public lasetNicknameUpdatedTime: Date
+
+    @prop()
+    public status: number
+}
+
+export const AlienModel = getModelForClass(Alien)
