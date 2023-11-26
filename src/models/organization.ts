f3ab1324cc8e987834b83ff35e2b761f39f82cf9
laggu91 <laggu91@gmail.com>
2023-11-26 10:18:26 +0000

    feat: organization query
    Merge-request: ARECIBO-MR-18
    Merged-by: 구원 나 <laggu91@gmail.com>

--- src/models/organization.ts
+++ src/models/organization.ts
@@ -1,5 +1,7 @@
+import { DeleteResult } from 'mongodb'
 import mongoose from 'mongoose'
-import { getModelForClass, prop } from '@typegoose/typegoose'
+import { getModelForClass, prop, ReturnModelType } from '@typegoose/typegoose'
+import { OrganizationNotFoundException } from '@/types/errors/database'
 
 export class Organization {
     public _id: mongoose.Types.ObjectId
@@ -24,6 +26,50 @@ export class Organization {
 
     @prop()
     public updatedAt: Date
+
+    public static async findByServerUrl(
+        this: ReturnModelType<typeof Organization>,
+        serverUrl: string,
+    ): Promise<Organization> {
+        return this.findByFilter({ serverUrl })
+    }
+
+    public static async findByClientId(
+        this: ReturnModelType<typeof Organization>,
+        clientId: string,
+    ): Promise<Organization> {
+        return this.findByFilter({ clientId })
+    }
+
+    public static async deleteByClientId(
+        this: ReturnModelType<typeof Organization>,
+        clientId: string,
+    ): Promise<DeleteResult> {
+        return this.deleteOne({ clientId: clientId })
+    }
+
+    public static async updateServerUrlByClientId(
+        this: ReturnModelType<typeof Organization>,
+        clientId: string,
+        newServerUrl: string,
+    ): Promise<void> {
+        await this.findOneAndUpdate(
+            { clientId: clientId },
+            { serverUrl: newServerUrl },
+        )
+    }
+
+    private static async findByFilter(
+        this: ReturnModelType<typeof Organization>,
+        filter: object,
+    ): Promise<Organization> {
+        const organization = await this.findOne(filter).exec()
+        if (organization) {
+            return organization
+        } else {
+            throw new OrganizationNotFoundException()
+        }
+    }
 }
 
 export const OrganizationModel = getModelForClass(Organization)
