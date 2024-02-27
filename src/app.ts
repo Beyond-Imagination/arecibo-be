db63eb0347169df9c4ad85ef58256fc2ddc3dcad
laggu91 <laggu91@gmail.com>
2024-02-27 14:09:07 +0000

    feat: gracefull shutdown
    Merge-request: ARECIBO-MR-150
    Merged-by: 구원 나 <laggu91@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import '@/config'
 import API from '@/api'
-import connect from '@/models/connector'
+import * as db from '@/models/connector'
+import { logger } from '@/utils/logger'
 ;(async () => {
-    await connect()
+    await db.connect()
     const api = new API()
 
     api.listen()
+
+    async function shutdown() {
+        logger.info('gracefully shutdown orbit')
+        await Promise.all([api.close])
+        await db.close()
+        logger.info('shutdown complete')
+        process.exit()
+    }
+
+    process.on('SIGINT', shutdown)
+    process.on('SIGTERM', shutdown)
 })()
