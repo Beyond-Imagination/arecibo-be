34109a217980330b651561d4ac04478b6363bdd9
laggu91 <laggu91@gmail.com>
2024-03-04 14:25:23 +0000

    feat: record metric
    database 에 저장된 document 개수를 메트릭으로 기록합니다
    메트릭은 뉴렐릭에서 대시보드를 만들떄 사용됩니다
    
    Merge-request: ARECIBO-MR-152
    Merged-by: 구원 나 <laggu91@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import '@/config'
 import API from '@/api'
+import Metric from '@/metric'
 import * as db from '@/models/connector'
 import { logger } from '@/utils/logger'
 ;(async () => {
     await db.connect()
     const api = new API()
+    const metric = new Metric()
 
     api.listen()
+    metric.run()
 
     async function shutdown() {
         logger.info('gracefully shutdown orbit')
-        await Promise.all([api.close])
+        await Promise.all([api.close, metric.stop])
         await db.close()
         logger.info('shutdown complete')
         process.exit()
---
+++ src/metric.ts
@@ -0,0 +1,42 @@
+import newrelic from 'newrelic'
+import schedule from 'node-schedule'
+
+import { logger } from '@/utils/logger'
+import { OrganizationModel } from '@/models/organization'
+import { AlienModel } from '@/models/alien'
+import { MessageModel } from '@/models/message'
+
+export default class Metric {
+    private async organizationCount() {
+        const count = (await OrganizationModel.estimatedDocumentCount()) as number
+        newrelic.recordMetric('organization/count', count)
+    }
+
+    private async alienCount() {
+        const count = (await AlienModel.estimatedDocumentCount()) as number
+        newrelic.recordMetric('alien/count', count)
+    }
+
+    private async messageCount() {
+        const count = (await MessageModel.estimatedDocumentCount()) as number
+        newrelic.recordMetric('message/count', count)
+    }
+
+    //TODO: comment 개수 metric 저장
+
+    public run() {
+        schedule.scheduleJob('0 0 * * *', async () => {
+            try {
+                await this.alienCount()
+                await this.organizationCount()
+                await this.messageCount()
+            } catch (e) {
+                logger.error('metric record fail', { error: e })
+            }
+        })
+    }
+
+    public async stop() {
+        await schedule.gracefulShutdown() // wait finish current job
+    }
+}
