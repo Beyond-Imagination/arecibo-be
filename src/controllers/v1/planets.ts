67e1b8dbf75ac2e1703886bb1b8ad340b59fd2ff
미나 이 <chsjen492@gmail.com>
2024-03-21 15:57:42 +0000

    refactor: modify function name and add middleware

--- src/controllers/v1/planets.ts
+++ src/controllers/v1/planets.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express'
 import asyncify from 'express-asyncify'
 import { PlanetModel } from '@/models/planet'
+import { verifyAlien } from '@/middlewares/aliens'
 
 const router = asyncify(express.Router({ mergeParams: true }))
 
-router.get('/', async (req: Request, res: Response) => {
-    const planets = await PlanetModel.findPlanetList()
+router.get('/', verifyAlien, async (req: Request, res: Response) => {
+    const planets = await PlanetModel.findSubscribablePlanetList()
     res.status(200).json({ planets: [...planets] })
 })
 export default router
